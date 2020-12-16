98157be7ca68bd4a56319c151babc575d879259b
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-12-16 13:43:11 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(function(s) {return s.result === "W" })
+  if (win) {
+    return win.year
+  } else {
+    return win
+  }
+
+
+}
