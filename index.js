72120896f2d7561daabfc14d4456308af2681821
HeartyRhombus <kaylahmg@gmail.com>
2020-12-18 09:06:14 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let result = record.find(game => game.result === "W")
+  return !!result ? result.year : undefined
+}
+
