d8f34e4f639d36de7e357536c11efa69c1c4cbdf
LeahESc <leahemily1@gmail.com>
2020-12-18 13:58:32 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(array) { 
+  let win = array.find( (w) => {
+    return w.result === "W"
+  })
+  if (win) { 
+    return win.year
+  } else { 
+    return win
+  }
+}
