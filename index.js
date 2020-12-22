3f60ce99c49f424768349e00568db6ef9dfac095
cailinkless <cailin.nysff@gmail.com>
2020-12-22 12:28:30 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let victory = record.find( function(x) { return x.result === "W" } )
+  if (!!victory == true) {
+    return victory.year
+  }
+}
