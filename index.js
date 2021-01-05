9d39b6909827b9db5a6233f00717d8612a202aad
David <dhutchis68516@gmail.com>
2021-01-05 15:35:10 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let win = record.find((w)=> {return w.result === "W"})
+  if (win) {
+    return win.year
+  } else {
+    return win
+  }
+  }
